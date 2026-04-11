6d17db49cc4b004aaa62d0c424fb941b39d9b465
Nguyen Khanh Son <ksondeptrai2k5@gmail.com>
2026-04-11 22:23:18 +0700

    feat: add IsSuspended field to Member model with descriptive comments

--- internal/models/member.go
+++ internal/models/member.go
@@ -15,6 +15,9 @@ type Member struct {
 	Level    string             `bson:"level" json:"level"`
 
 	IsRegistered          bool      `bson:"is_registered" json:"is_registered"`
+	// Using bool IsSuspended to indicate if the member is suspended or not. If true, the member is suspended and cannot attend sessions
+	// and cannot suspend in the future. If false, the member is not suspended and can attend sessions and can suspend in the future.
+	IsSuspended           bool      `bson:"is_suspended" json:"is_suspended"`
 	TotalSessionsAttended int       `bson:"total_sessions_attended" json:"total_sessions_attended"`
 	CreatedAt             time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt             time.Time `bson:"updated_at" json:"updated_at"`
